c594ae2454374401ad94b195e95cca3d9def1d7a
Vitor Oliveira Dos Santos <51249323+vitor-o-s@users.noreply.github.com>
2020-07-28 12:18:04 -0300

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -5,10 +5,10 @@ void insertion(int n, int array[]){
     value = array[i];
     j = i - 1;
     /* Move elements of arr[0..i-1], that are
-      greater than key, to one position ahead
+      greater than value, to one position ahead
       of their current position */
     while(value < array[j] && j >=0){
-      array[j + 1] = array[j];
+      array[j+1] = array[j];
       j--;
     }
     array[j+1] = value;
