220821c3cad9b44d04fc052de323d0d6523113a9
Vitor Oliveira Dos Santos <51249323+vitor-o-s@users.noreply.github.com>
2020-07-30 14:16:25 -0300

    add arquivo de arvore para submissao hackerrank

--- tree.c
+++ tree.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Leaf{
   int value;
@@ -15,7 +15,6 @@ void init(tree* t){
   t->root = NULL;
 }
 
-// A utility function to create a new BST leaf
 leaf* create_leaf(int value){
   leaf *new = (leaf *) malloc (sizeof(leaf));
   if(new == NULL) return NULL;
@@ -24,70 +23,47 @@ leaf* create_leaf(int value){
   return new;
 }
 
-// A utility function to do inorder traversal of BST
-void inorder(leaf *root){
+void inorder(leaf *root){    
 
     if (root != NULL){
       inorder(root->left);
-      printf(" %d \t", root->value);
+        if(root->left == NULL && root->right == NULL){
+            printf(" %d", root->value);
+        }        
       inorder(root->right);
-    }
+    }    
 }
 
-leaf* search(leaf* root, int value){
-    // Base Cases: root is null or key is present at root
-    if (root == NULL || root->value == value)
-       return root;
-
-    // Key is greater than root's key
-    if (root->value < value)
-       return search(root->right, value);
-
-    // Key is smaller than root's key
-    return search(root->left, value);
-}
-
-// A utility function to insert a new node with given key in BST
 leaf* insert(leaf *root, int value){
 
-  // If the tree is empty, return a new node
   if (root == NULL) return create_leaf(value);
 
-  // Otherwise, recur down the tree
   if (value < root->value)
       root->left  = insert(root->left, value);
   else if (value > root->value)
       root->right = insert(root->right, value);
 
-  // return the (unchanged) node pointer
   return root;
 
 }
 
-// Driver Program to test above functions
-int main(void){
-
-  // Let us create following BST
-  //         50
-  //        |  |
-  //       30  70
-  //      | |  | |
-  //     20 40 60 80
+int main() {
+    
+    int n;
+    scanf(" %d",&n);
 
     tree *t = (tree*) malloc (sizeof(tree));
-
     init(t);
-
-    t->root = insert(t->root, 50);
-    insert(t->root, 30);
-    insert(t->root, 20);
-    insert(t->root, 40);
-    insert(t->root, 70);
-    insert(t->root, 60);
-    insert(t->root, 80);
-
-    // print inoder traversal of the BST
+    int i, key;
+    scanf(" %d",&key);
+    t->root = insert(t->root, key);
+    
+    for(i=1; i<n; i++){
+        scanf(" %d",&key);
+        insert(t->root, key);
+    }
     inorder(t->root);
-
+    
     return 0;
 }
+
