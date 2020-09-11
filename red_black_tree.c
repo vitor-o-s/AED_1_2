8f9a3256e9c884a73bd0301d0a77f0e93a1d823c
Vitão <vitorsantos.2018@alunos.utfpr.edu.br>
2020-09-10 23:13:06 -0300

    Add balance/black_height

--- red_black_tree.c
+++ red_black_tree.c
@@ -10,17 +10,14 @@ typedef struct Leaf{
   struct Leaf *right;
   struct Leaf *parent;
   int color;
-  int balance_value;
 }leaf;
 
 typedef struct Tree{
   leaf *root;
-  int balance;
 }tree;
 
 void init(tree* t){
   t->root = NULL;
-  t->balance = 0;
 }
 
 leaf* create_leaf(int value){
@@ -32,31 +29,185 @@ leaf* create_leaf(int value){
   return new;
 }
 
-leaf* insert(tree* root, int value){
+void inorder(leaf *root){
 
-  if(root == NULL){
-    return create_leaf(value);
-  }
-  if(value < root->value){
-    root->left = insert(root->left,value);
-    root->left->parent = root;
-  }
-  if(value > root->value){
-    root->right = insert(root->right,value);
-    root->right->parent = root;
-  }
+    if (root != NULL){
+      inorder(root->left);
+      printf("%d %d\t", root->value,root->color);
+      inorder(root->right);
+    }
+}
 
+leaf* uncle(leaf* root){
+  leaf* aux = NULL;
+
+  if((root != NULL) && (root->parent != NULL)){
+    if (root->parent->parent->left == root->parent){
+      aux = root->parent->parent->right;
+    }
+    else{
+      aux = root->parent->parent->left;
+    }
+  }
+  return aux;
+}
+
+leaf* grandp(leaf *root){
+
+  if((root != NULL) && (root->parent!= NULL)) return  root->parent->parent;
+
+  return NULL;
+}
+
+leaf* rot_right(leaf *root){
+  leaf *p = root->parent, *g = p->parent;
+
+  p->parent = g->parent;
+  g->parent = p;
+  g->left   = p->right;
+  p->right  = g;
+
+  root = p;
   return root;
 }
 
-int maior(int a, int b){
-  if(a>b) return a;
-  return b;
+leaf* rot_left(leaf *root){
+  leaf *p = root->parent, *g = p->parent;
+
+  p->parent = g->parent;
+  g->parent = p;
+  g->right  = p->left;
+  p->left   = g;
+
+  root = p;
+  return root;
+}
+
+leaf* rot_right_left(leaf *root){
+  leaf *p = root->parent, *g = p->parent;
+
+  root->parent = p->parent;
+  p->parent    = root;
+  p->left      = root->right;
+  root->right  = p;
+
+  return rot_left(p);
+}
+
+leaf* rot_left_right(leaf *root){
+  leaf *p = root->parent, *g = p->parent;
+
+  root->parent = p->parent;
+  p->parent    = root;
+  p->right     = root->left;
+  root->left   = p;
+
+  return rot_right(p);
+}
+
+void balance(leaf* root){
+
+  leaf *p, *u; //parente, uncle
+
+  if(root->parent == NULL){ //case 1
+    //printf("entrou caso 1\n");
+    root->color = BLACK;
+  }
+  else{
+    p = root->parent;
+
+    if(p->color == RED){
+
+      u = uncle(root);
+
+      if((u != NULL) && (u->color == RED)){//case 2
+        //printf("entrou caso 2\n");
+        p->color = u->color = BLACK;
+        //printf("valor pai: %d cor: %d valor do tio:%d cor:%d\n", p->value, p->color,u->value,u->color);
+        root = grandp(root);
+
+        if(root->parent != NULL)
+        root->color = RED;
+      }
+
+      else{//case 3
+        if(p->left == root){
+          if(p->parent->right == p){
+            //printf("entrou caso 3c\n");
+            rot_right_left(root);//case 3c
+            //inorder(root);
+          }
+          else{
+            //printf("entrou caso 3a\n");
+            rot_right(root); //case 3
+          }
+        }
+        else{
+          if(p->parent->right == p){
+            //printf("entrou caso 3d\n");
+            rot_left_right(root);//case 3d
+          }
+          else{
+            //printf("entrou caso 3b\n");
+            rot_left(root); //case 3b
+          }
+        }
+        //printf("fazendo pintura dos nós\n");
+      //  printf("%d %d\n",root->value,root->parent->value);
+        root->color = BLACK;
+        if(root->left  != NULL) root->left->color  = RED;
+        if(root->right != NULL)root->right->color = RED;
+        //printf("saiu balance\n");
+
+      }
+    }
+  }
+
+}
+
+leaf* insert(leaf* root, int value){
+
+  leaf *auxP, *auxF;
+
+  if (root == NULL){
+      return create_leaf(value);
+  }
+
+  else{
+    auxP = auxF = root;
+
+    while (auxF != NULL){
+      auxP = auxF;
+
+      if (value < auxF->value)
+      auxF = auxF->left;
+      else
+      auxF = auxF->right;
+    }
+
+    auxF = create_leaf(value);
+    auxF->parent = auxP;
+
+    if (auxF->value < auxP->value)
+    auxP->left = auxF;
+    else
+    auxP->right = auxF;
+
+    while ((auxF->parent != NULL)  && (auxF->color == RED) && (auxF->parent->color == RED)){
+      //printf("entrou balance\n");
+      balance(auxF);
+    }
+
+  }
+  return root;
 }
 
 int bh(leaf* root){
-
+  if(root== NULL) return 0;
+  if(root->color == BLACK) return 1 + bh(root->left);
+  return bh(root->left);
 }
+
 
 
 int main(void){
@@ -69,19 +220,16 @@ int main(void){
   int i, key;
   scanf(" %d",&key);
   t->root = insert(t->root, key);
+  t->root->color = BLACK;
+
 
   for(i=1; i<n; i++){
       scanf(" %d",&key);
       insert(t->root, key);
   }
-  //Inserir ordenado ou ordenar depois
-  while(t->balance > 1 || t->balance < -1){
-    //Reorganiza
-    //calcula fator de balanceamento
 
-  }
-
-  printf("%d\n",maior(bh(t->root->left),bh(t->root->left)));
+  //inorder(t->root);
+  printf("%d\n",bh(t->root));
 
   return 0;
 }
