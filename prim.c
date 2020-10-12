b5a2ef9d7b0bfd383e7bade543a45744b442ffc6
Vitão <vitorsantos.2018@alunos.utfpr.edu.br>
2020-10-11 22:49:24 -0300

    fix unstack

--- prim.c
+++ prim.c
@@ -6,43 +6,26 @@
 static int stack[N];
 static int last, first;
 
-typedef struct Edge{
-  int value, weight;
-  struct Edge* next;
-}edge;
-
-typedef struct Node{
-  int value;
-  struct edge* edge_next;
-  struct Node* next;
-}node;
-
-typedef struct list{
-  node* head;
-  int n_vertex;
-}List;
-
 int unstack(int n, int chave[n]){
 
-  int menor = 10000, i, indice;
+  int menor = 10000, i, j;
 
-  for ( i = first; i < last; i++) {
-    if (chave[i] < menor){
-      menor = chave[i];
-      indice = i;
-    }
+  i = first;
+  while(i < last && chave[i] > menor){
+    i++;
   }
-  int vertice = stack[indice];
 
-  if(indice == first) first++;
-  if(indice == last) last--;
+  int vertice;
+  vertice = stack[i];
 
-  if(indice != first && indice != last){
-    for ( i = indice; i < last; i++) {
-      stack[i] = stack[i + 1];
+  if(i != first && i != last){
+    for ( j = i; j < last; j++) {
+      stack[j] = stack[j + 1];
     }
     last--;
   }
+  if(i == first) first++;
+  if(i == last) last--;
 
   return vertice;
 }
@@ -58,14 +41,7 @@ int isvoid(void){
 }
 
 void createstack(void){
-   first = last = 0;
-}
-
-void printstack(void){
-  int i;
-  for(i = first; i < last; i++)
-    printf("%d\t", stack[i]);
-  printf("\n");
+  first = last = 0;
 }
 
 int Estack(int n){
@@ -78,108 +54,7 @@ int Estack(int n){
   return 0;
 }
 
-/*
-void inicializa(List* l){
-  l->head = NULL;
-}
-
-node * create_node(int value){
-  node *new;
-  new = (node *) malloc (sizeof(node));
-  if(new == NULL) return NULL;
-  new->value = value;
-  new->edge_next = NULL;
-  new->next = NULL;
-  return new;
-}
-
-edge * create_edge(int value, int weight){
-  edge *new;
-  new = (edge *) malloc (sizeof(edge));
-  if(new == NULL) return NULL;
-  new->value = value;
-  new->weight = weight;
-  new->next = NULL;
-  return new;
-}
-
-void addV(List *l, int value){
-  //add no fim da lista
-  node* new = create_node(value);
-  if(l->head==NULL){
-    l->head = new;
-    return;
-  }
-  node* aux = l->head;
-
-  while(aux->next!=NULL){
-    //não permite criação de valores repetidos
-    if(aux->value == value) return;
-    aux = aux->next;
-  }
-  if(aux->value == value) return;
-  aux->next = new;
-  return;
-}
-
-void imprimirGrafo(List *l){
-  node * aux = l->head;
-  while(aux!=NULL){
-    //Imprime o Vertice
-    printf(" %d: ",aux->value);
-    //Imprime lista de arestas
-    edge* edge_aux;
-    edge_aux = aux->edge_next;
-    while (edge_aux!=NULL) {
-      printf(" %d, peso: %d",edge_aux->value,edge_aux->weight);
-      edge_aux = edge_aux->next;
-    }
-    aux = aux->next;
-    printf("\n");
-  }
-}
-
-void w_add(List *l, int V, int A, int W){
-  node* aux = l->head;
-  edge* new = create_edge(A, W);
-  //Busca vertice
-  while(aux->value!=V){
-    aux = aux->next;
-  }
-  //Add ini
-  if(aux->edge_next == NULL){
-    aux->edge_next = new;
-    return;
-  }
-  //Add fim
-  edge* edge_aux = aux->edge_next;
-
-  while(edge_aux->next!=NULL){
-    edge_aux = edge_aux->next;
-  }
-  edge_aux->next = new;
-  return;
-}
-
-List* adj_list(List *l, int n, int matrix[n][n]){
-
-  int i, j;
-
-  for(i = 0; i < n; i++)
-    addV(l,i);
-
-  for(i=0; i<n; i++){
-    for(j=0; j<n; j++){
-      if(matrix[i][j] != 0)
-        w_add(l,i,j,matrix[i][j]);
-    }
-  }
-  return l;
-}
-
-*/
-
-void prim(int n, int root, int matrix[n][n]){
+void prim(int n, int matrix[n][n]){
 
   int pai[n], chave[n], i, u;
 
@@ -193,10 +68,8 @@ void prim(int n, int root, int matrix[n][n]){
     stackup(i);
   }
 
-
-
   while(isvoid() != 0){
-    u = unstack(n, chave); // alterar função para desempilhar menor chave
+    u = unstack(n, chave);
     for ( i = 0; i < n; i++) {
       if (matrix[u][i] != 0) {
         if(Estack(i) == 1 && matrix[u][i] < chave[i]){
@@ -224,31 +97,7 @@ int main(void) {
     }
   }
 
-  //List *L = (List*) malloc (sizeof(List));
-  //List *arvore_geradora = (List *) malloc (sizeof(List));
-  //inicializa(L);
-  //inicializa(arvore_geradora);
-
-  //L = adj_list(L, n, adj_matrix);
-  //imprimirGrafo(L);
-  //arvore_geradora =
-  printf("leu");
-  prim(n, 0, adj_matrix);
+  prim(n, adj_matrix);
 
   return 0;
 }
-
-/* OUTPUT ESPERADO
-
-
-0: -
-1: 0
-2: 1
-3: 2
-4: 3
-5: 2
-6: 5
-7: 6
-8: 2
-
-*/
